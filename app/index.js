836f4248309bba1cc0e113991cf94e08135b4dd4
vagrant <vagrant@servidorUbuntu>
2022-09-01 17:54:22 +0000

    first commit consul service

---
+++ app/index.js
@@ -0,0 +1,70 @@
+const Consul = require('consul');
+const express = require('express');
+
+const SERVICE_NAME='microservicio101';
+const SERVICE_ID='m'+process.argv[2];
+const SCHEME='http';
+const HOST='192.168.100.3';
+const PORT=process.argv[2]*1;
+const PID = process.pid;
+
+/* Inicializamos server */
+const app = express();
+const consul = new Consul();
+
+app.get('/health', function (req, res) {
+    console.log('Health check!');
+    res.end( "Ok." );
+    });
+
+/*
+app.get('/', function (req, res) {
+    var s="<h1>Instancia '"+SERVICE_ID+"' del servicio '"+SERVICE_NAME+"'</h1>";
+    s+="<h2>Listado de servicios</h2>";
+
+    console.log(s);
+
+    consul.agent.service.list(function(err, result) {
+      if (err) {
+	      console.log('Service List Error');
+	      throw err;}
+
+      console.log('Antes de');
+      res.end( s+JSON.stringify( result ) );
+      console.log(result);
+
+      });
+    });
+ 
+*/
+
+app.get('/', (req, res) => {
+  console.log('GET /', Date.now());
+  res.json({
+    data: Math.floor(Math.random() * 89999999 + 10000000),
+    data_pid: PID
+  });
+});
+
+app.listen(PORT, function () {
+    console.log('Sistema armado en el puerto '+SCHEME+'://'+HOST+':'+PORT+'!');
+    });
+
+/* Registro del servicio */
+var check = {
+  id: SERVICE_ID,
+  name: SERVICE_NAME,
+  address: HOST,
+  port: PORT, 
+  check: {
+	   http: SCHEME+'://'+HOST+':'+PORT+'/health',
+	   ttl: '5s',
+	   interval: '5s',
+     timeout: '5s',
+     deregistercriticalserviceafter: '1m'
+	   }
+  };
+ 
+consul.agent.service.register(check, function(err) {
+  	if (err) throw err;
+  	});
