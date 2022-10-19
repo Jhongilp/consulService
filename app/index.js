e4a6056ea37da65e22eb70a4457bd4007d530c78
Jonathan Gil <jhongilp@gmail.com>
2022-10-19 08:46:35 -0500

    Update Consul service name

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const Consul = require('consul');
 const express = require('express');
 
-const SERVICE_NAME='mymicroservice';
+const SERVICE_NAME='web';
 const SERVICE_ID='m'+process.argv[2];
 const SCHEME='http';
 const HOST=process.env.VM_HOST;
